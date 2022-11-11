39b7c25eefce46d14d9711a64332be77befd63eb
sophiekedrowski <105030180+sophiekedrowski@users.noreply.github.com>
2022-11-10 18:05:28 -0600

    Added routes to index and notes file

--- server.js
+++ server.js
@@ -7,12 +7,19 @@ const PORT = 3001;
 //app.use(express.static('public'));
 
 const app = express();
+// app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('Visit http://localhost:3001/api'));
+// app.get('/', (req, res) => res.send('Visit http://localhost:3001/api'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
   });
+
+
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+  });
+
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
